a0b7e33aa8a36de9f955933babb00e98a17c4fd1
Christopher Partin <christopherpartin11@gmail.com>
2024-07-15 16:13:38 -0700

    Created quests.py and added the methods for title, quest type, monetary reward, other reward, and description.
    also added more to the questboard and added the Quest class.

--- questboard.py
+++ questboard.py
@@ -4,4 +4,19 @@ class QuestBoard:
         self._available_quests = available_quests
         self._completed_quests = completed_quests
 
+    def get_current_quests(self):
+        """returns current_quests (DICT)"""
+        return self._current_quests
+
+    def get_available_quests(self):
+        """returns available_quests (DICT)"""
+        return self._available_quests
+    
+    def get_completed_quests(self):
+        """returns completed quests (DICT)"""
+        return self._completed_quests
+    
+
+
+
     
---
+++ quests.py
@@ -0,0 +1,28 @@
+class Quest:
+    def __init__(self, title, quest_type, monetary_reward, other_reward, description):
+        self._title = title
+        self._quest_type = quest_type
+        self._monetary_reward = monetary_reward
+        self._other_reward = other_reward
+        self._description = description
+
+    def get_title(self):
+        """returns the Quest title (STR)"""
+        return self._title
+    
+    def get_quest_type(self):
+        """returns the Quest type (STR)"""
+        return self._quest_type
+    
+    def get_monetary_reward(self):
+        """returns the Quest monetary reward (INT)"""
+        return self._monetary_reward
+    
+    def get_other_reward(self):
+        """returns the Quest other reward (if there is one) (either cat OBJ, item OBJ, or NONE)"""
+        return self._other_reward
+    
+    def get_description(self):
+        """returns the Quest description (STR)"""
+        return self._description
+
