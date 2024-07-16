f3794b87106c5573e744bd0fabb7156ed57b1704
Christopher Partin <christopherpartin11@gmail.com>
2024-07-16 12:42:50 -0700

    Added more functionality to the different classes. Added functions to Customer in customer.py and some more features in distributor.py.

--- customer.py
+++ customer.py
@@ -1,9 +1,55 @@
 class Customer:
-    def __init__(self, name, fave_genre, fave_cat_type, currency_level, books_to_sell):
+    def __init__(self, name, relationship_level, relationship_points, fave_genre,
+                 fave_cat_type, fave_snack, currency_level, price_tolerance, books_to_sell):
         self._name = name
+        self._relationship_level = relationship_level
+        self._relationship_points = relationship_points
         self._fave_genre = fave_genre
         self._fave_cat_type = fave_cat_type
+        self._fave_snack = fave_snack
         self._currency_level = currency_level
+        self._price_tolerance = price_tolerance
         self._books_to_sell = books_to_sell
+
+    def get_name(self):
+        """returns Customer name (STR)"""
+        return self._name
+    
+    def get_relationship_level(self):
+        """returns Customer relationship_level (INT)"""
+        return self._relationship_level
+    
+    def get_relationship_points(self):
+        """returns Customer relationship_points (INT)"""
+        return self._relationship_points
+    
+    def get_fave_genre(self):
+        """returns Customer fave genre (STR)"""
+        return self._fave_genre
+    
+    def get_fave_cat_type(self):
+        """returns Customer fave_cat_type (STR)"""
+        return self._fave_cat_type
+    
+    def get_fave_snack(self):
+        """returns Customer fave snack (STR)"""
+        return self._fave_snack
+    
+    def get_currency_level(self):
+        """returns Customer currency_level (INT)"""
+        return self._currency_level
+    
+    def get_price_tolerance(self):
+        """returns Customer price_tolerance (STR)"""
+        return self._price_tolerance
+    
+    def get_books_to_sell(self):
+        """"returns Customer books_to_sell (LIST)"""
+        return self._books_to_sell
+    
+    def add_book_to_sell(self, book: object):
+        """adds a book to the books_to_sell list"""
+        self._books_to_sell.append(book)
+
 
 
--- distributor.py
+++ distributor.py
@@ -1,2 +1,30 @@
 class Distributor:
-    pass
+    def __init__(self, book_inventory, rare_inventory):
+        self._book_inventory = book_inventory
+        self._rare_inventory = rare_inventory
+
+    def get_book_inventory(self):
+        """returns book inventory (DICT)
+        {genre1: [book1, book2], genre2: [book3, book4]}"""
+        return self._book_inventory
+    
+    def add_book_to_inventory(self, book):
+        """add book item object to Distributor book_inventory (DICT)"""
+        self._book_inventory[book.get_genre()].append(book)
+
+    def get_rare_inventory(self):
+        """returns Distributor rare_inventory (DICT)"""
+        return self._rare_inventory
+    
+    def add_item_to_rare_inventory(self, item):
+        """add item object to Distributor rare_inventory (DICT)"""
+        self._rare_inventory[item.get_genre()].append(item)
+
+    def remove_item_from_rare_inventory(self, remove_item):
+        """remove item object from Distributor rare_inventory (DICT)"""
+        count = 0
+        for item in self._rare_inventory[remove_item.get_genre()]:
+            if item.get_name() == remove_item.get_name():
+                self._rare_inventory[remove_item.get_genre()].pop(count)
+            count += 1
+
